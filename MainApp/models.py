179fffe9a53697854bbcf5eb4c4c0f14aab3d000
student <v77_@list.ru>
2023-05-26 17:38:59 +0300

    new lang stracture, but need new filtering

--- MainApp/models.py
+++ MainApp/models.py
@@ -1,21 +1,31 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-LANGS = (
-    ('py', 'python'),
-    ('js', 'JavaScript'),
-    ('cpp', 'C++'),
-)
+# LANGS = (
+#     ('py', 'python'),
+#     ('js', 'JavaScript'),
+#     ('cpp', 'C++'),
+# )
+
+class Language(models.Model):
+    name = models.CharField(max_length=32)
+    # short_name = models.CharField(max_length=8)
+
+    def __str__(self):
+        return f"{self.name}"
+
 
 class Snippet(models.Model):
     name = models.CharField(max_length=100)
-    lang = models.CharField(max_length=30, choices=LANGS)
+    # lang = models.CharField(max_length=30, choices=LANGS)
+    lang = models.ForeignKey(to=Language, on_delete=models.PROTECT,
+                             null=True, blank=True)
     code = models.TextField(max_length=5000)
     creation_date = models.DateTimeField(auto_now=True)
     user = models.ForeignKey(to=User, on_delete=models.CASCADE,
                              blank=True, null=True)
     private = models.BooleanField(default=True)
-    
+
     def __str__(self):
         return f"Snippet{self.name} | {self.user}"
 
